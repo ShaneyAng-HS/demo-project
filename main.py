f060c49021d80c218b7c6e80c547c42adb32e038
ShaneyAng-HS <shaney_ang@yahoo.com>
2025-06-30 17:21:47 +0800

    create class

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 # Import libraries
 import pandas as pd
-from src.preprocessing import clean_data
+from src.preprocessing import DataPreparation
+import yaml
 
 def main():
+
+    # Configuration file path
+    config_path = "./src/config.yaml"
+
+    with open(config_path, "r") as file:
+        config = yaml.safe_load(file)
     
-    data_dict = { 'Cust_ID': [1,2,3], 
-                'Name': ['Amos', 'Shaney', 'Lionel'],
-                'Amount': ['10','15','5'],
-                'Item': ['T-shirt', 'Shorts', 'Hat'],
+    data_dict = { 'Cust_ID': [1,2,3,4], 
+                'Name': ['Amos', 'Shaney', 'Lionel', 'Ameer'],
+                'Amount': ['10','15','5', '25'],
+                'Item': ['T-shirt', 'Shorts', 'Hat', 'Coat'],
+                'Merchant': ['H&M', 'Uniqlo', 'UNiQlO', 'H2M'],
                 }
     df = pd.DataFrame(data_dict)
-    df = clean_data(df)
+
+    # Initialize and run data preparation
+    data_prep = DataPreparation(config)
+    df = data_prep.clean_data(df)
+
     print(df)
     df.info()
 
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,12 +1,32 @@
 # Import libraries
 import pandas as pd
+from typing import Any, Dict
 
 # Write helper functions
 
-def clean_data(df):
-    
-    df['Amount'] = df['Amount'].astype(int)
+# def clean_data(df):
 
-    return df
+#     df['Amount'] = df['Amount'].astype(int)
 
+#     return df
 
+class DataPreparation:
+
+    def __init__(self, config: Dict[str, Any]):
+
+        self.config = config
+
+    @staticmethod
+    def _clean_merchant_name(name: str) -> str:
+
+        name = name.capitalize()
+        name = name.replace('2','&')
+        
+        return name
+
+    def clean_data(self, df):
+
+        df['Amount'] = df['Amount'].astype(int)
+        df['Merchant'] = df['Merchant'].apply(self._clean_merchant_name)
+
+        return df
